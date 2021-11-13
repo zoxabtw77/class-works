a82ee28d5655714d59075fb53ab967b2041216f5
Zoxir Rustamov <zoxabtw77@gmail.com>
2021-11-13 19:56:27 +0500

    class works5

---
+++ index.py
@@ -0,0 +1,23 @@
+# name = input("enter your name: ")
+# surname = input("enter your last name: ")
+
+# print ("Hello, " + name + surname)
+# print(f"Hello, {name} {surname}")
+# print('Hello, {} {}' .format(name, surname))
+
+#Hello name, surname 
+
+# num1 = int(input('Enter 1st number:'))
+# num2 = int(input('Enter 2nd number:'))
+
+# print(f'{num1} + {num2} = {num1+num2}')
+
+# 13+13=26
+
+# num1 = int(input('Enter 1st number:'))
+# num2 = int(input('Enter 2nd number:'))
+
+# print(f'{num1} * {num2} = {num1*num2}')   #3*10=30
+
+# print('Im\zoxabtw')
+# print("I'm zoxabtw")
